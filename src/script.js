2e348449804f9ea0b41e1946071b0696ef939371
maryluzmeva <maryluzmeva@gmail.com>
2023-08-18 19:46:35 +0200

    Update the Function to work with every city

--- src/script.js
+++ src/script.js
@@ -1,26 +1,12 @@
 function showDate(event) {
-    if (event.target.value.length > 0) {
-        if (event.target.value === "paris") {
-          let parisTime = moment()
-            .tz("Europe/Paris")
-            .format("[It is] dddd, MMMM D, H:mm A [in Europe/Paris]");
-          alert(`${parisTime}`);
-        }
-        if (event.target.value === "tokyo") {
-          let tokyoTime = moment()
-            .tz("Asia/Tokyo")
-            .format("[It is] dddd, MMMM D, H:mm A [in Asia/Tokyo]");
-          alert(`${tokyoTime}`);
-        }
-        if (event.target.value === "sydney") {
-          let sydneyTime = moment()
-            .tz("Australia/Sydney")
-            .format("[It is] dddd, MMMM D, H:mm A [in Australia/Sydney]");
-          alert(`${sydneyTime}`);
-        }
-      }
-    }
-    let timezoneElement = document.querySelector("#timezone");
-    timezoneElement.addEventListener("change", showTimezone);
+  if (event.target.value.length > 0) {
+    let currentTime = moment()
+      .tz(event.target.value)
+      .format("dddd, MMMM D, YYYY h:mm A");
 
-    
+    alert(`It is ${currentTime} in ${event.target.value}`);
+  }
+}
+
+let selectElement = document.querySelector("#timezones");
+selectElement.addEventListener("change", showDate);
