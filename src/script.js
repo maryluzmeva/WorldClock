d2573b05949f3c4f2e1c138dd080ad1ae36f8243
maryluzmeva <maryluzmeva@gmail.com>
2023-08-18 18:55:09 +0200

    Moment and HTML

--- src/script.js
+++ src/script.js
@@ -1,12 +1,24 @@
 function showDate(event) {
     if (event.target.value.length > 0) {
-      let currentTime = moment()
-        .tz(event.target.value)
-        .format("ddd,MMMM D, YYYY HH:mm A");
-
-      alert(`It is ${currentTime} in ${event.target.value}`);
+        if (event.target.value === "paris") {
+          let parisTime = moment()
+            .tz("Europe/Paris")
+            .format("[It is] dddd, MMMM D, H:mm A [in Europe/Paris]");
+          alert(`${parisTime}`);
+        }
+        if (event.target.value === "tokyo") {
+          let tokyoTime = moment()
+            .tz("Asia/Tokyo")
+            .format("[It is] dddd, MMMM D, H:mm A [in Asia/Tokyo]");
+          alert(`${tokyoTime}`);
+        }
+        if (event.target.value === "sydney") {
+          let sydneyTime = moment()
+            .tz("Australia/Sydney")
+            .format("[It is] dddd, MMMM D, H:mm A [in Australia/Sydney]");
+          alert(`${sydneyTime}`);
+        }
+      }
     }
-  }
-
-  let timezones = document.querySelector("#timezones");
-  timezones.addEventListener("change", showDate);
+    let timezoneElement = document.querySelector("#timezone");
+    timezoneElement.addEventListener("change", showTimezone);
