0767031ee2866224fa1d5ae20aa3c6a018cdfb69
maryluzmeva <maryluzmeva@gmail.com>
2023-08-21 16:36:26 +0200

    Add Multiple Location

--- src/script.js
+++ src/script.js
@@ -39,7 +39,26 @@ function updateTime() {
   tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
   tokyoTimeElement.innerHTML = tokyoTime.format("hh:mm:ss [<small>]A[</small>]");
 }
+function updateCity (event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
+}
 
 updateTime();
 setInterval(updateTime, 1000);
 
+let citySelectElement=document.querySelector ("#city");
+citySelectElement.addEventListener ("change", updateCity);
