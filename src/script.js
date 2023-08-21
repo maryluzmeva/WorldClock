472f37ecea0f0e777bf62e4ba6673a61a59c1bd8
maryluzmeva <maryluzmeva@gmail.com>
2023-08-21 15:11:08 +0200

    Change the Transition and the CSS for the cities

--- src/script.js
+++ src/script.js
@@ -1,29 +1,4 @@
-function showDate(event) {
-  if (event.target.value.length > 0) {
-    let currentTime = moment()
-      .tz(event.target.value)
-      .format("dddd, MMMM D, YYYY h:mm A");
-
-    alert(`It is ${currentTime} in ${event.target.value}`);
-  }
-}
-
-let selectElement = document.querySelector("#timezones");
-selectElement.addEventListener("change", showDate);
-
 function updateTime() {
-  let tokyoElement = document.querySelector("#tokyo");
-  if (tokyoElement) {
-    let tokyoDateElement = tokyoElement.querySelector(".date");
-    let tokyoTimeElement = tokyoElement.querySelector(".time");
-    let tokyoTime = moment().tz("Asia/Tokyo");
-
-    tokyoDateElement.innerHTML = tokyoTime.format("MMMM	Do YYYY");
-    tokyoTimeElement.innerHTML = tokyoTime.format(
-      "h:mm:ss [<small>]A[</small>]"
-    );
-  }
-
   let berlinElement = document.querySelector("#berlin");
   if (berlinElement) {
     let berlinDateElement = berlinElement.querySelector(".date");
